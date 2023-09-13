13c3ed08b4a3e5a32d65a6804f73c2180f15641b
Maks <90788821+MaksKukarach@users.noreply.github.com>
2023-09-13 14:54:17 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,6 +2,6 @@ def count_words(text):
     words = text.split()
     return len(words)
 
-if __name__ == "__main__": # Типа по крутому сделал
+if __name__ == "__main__":
     text = input("Введите текст: ")
     print(f"Количество слов в тексте: {count_words(text)}")
